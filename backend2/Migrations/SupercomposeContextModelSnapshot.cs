4d0e73e557ff8feb8b4ef9e736ae067afc9fdde4
Jan Kelin <jan@jkelin.com>
2021-03-02 08:09:51 +0100

    Fix migrations

--- backend2/Migrations/SupercomposeContextModelSnapshot.cs
+++ backend2/Migrations/SupercomposeContextModelSnapshot.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
-using backend2;
+using supercompose;
 
 namespace supercompose.Migrations
 {
