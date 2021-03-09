b13ef00f48222820877d58810a0d06d319c8af31
Jan Kelin <jan@jkelin.com>
2021-03-09 09:05:45 +0100

    Improve error logging

--- backend2/Util/ConsoleQueryLogger.cs
+++ backend2/Util/ConsoleQueryLogger.cs
@@ -20,87 +20,26 @@ namespace supercompose
       _logger = logger;
     }
 
-    // this diagnostic event is raised when a request is executed ...
-    public override IActivityScope ExecuteRequest(IRequestContext context)
-    {
-      // ... we will return an activity scope that is used to signal when the request is
-      // finished.
-      return new RequestScope(_logger, context);
-    }
-
     public override void ResolverError(IMiddlewareContext context, IError error)
     {
-      base.ResolverError(context, error);
       if (error.Exception != null)
-        _logger.LogError(new EventId(1, "GraphQL Error"), error.Exception, "GraphQL Error in {name}",
-          context.Field.Name);
-    }
-
-    private class RequestScope : IActivityScope
-    {
-      private readonly IRequestContext _context;
-      private readonly ILogger<ConsoleQueryLogger> _logger;
-
-      public RequestScope(ILogger<ConsoleQueryLogger> logger, IRequestContext context)
       {
-        _logger = logger;
-        _context = context;
-        _queryTimer = new Stopwatch();
-        _queryTimer.Start();
-      }
-
-      public void Dispose()
-      {
-        // when the request is finished it will dispose the activity scope and
-        // this is when we print the parsed query.
-        if (_context.Document is not null)
+        using var scope = _logger.BeginScope(new
         {
-          // we just need to do a ToString on the Document which represents the parsed
-          // GraphQL request document.
-          StringBuilder stringBuilder = new(_context.Document.ToString(true));
-          stringBuilder.AppendLine();
+          Resolver = $"{context.Field.DeclaringType.Name}.{context.Field.Name}",
+          Document = context.Document.ToString().Replace("\n", Environment.NewLine),
+          Variables = string.Join(Environment.NewLine, context.Variables.Select(x => $"{x.Name}: {x.Value}"))
+        });
 
-          if (_context.Variables != null)
-          {
-            var variablesConcrete = _context.Variables!.ToList();
-            if (variablesConcrete.Count > 0)
-            {
-              stringBuilder.AppendFormat($"Variables {Environment.NewLine}");
-              try
-              {
-                foreach (var variableValue in _context.Variables!)
-                {
-                  if (variableValue.Name == "password" || variableValue.Name == "privateKey") continue;
-
-                  string PadRightHelper(string existingString, int lengthToPadTo)
-                  {
-                    if (string.IsNullOrEmpty(existingString)) return "".PadRight(lengthToPadTo);
-
-                    if (existingString.Length > lengthToPadTo) return existingString.Substring(0, lengthToPadTo);
-
-                    return existingString + " ".PadRight(lengthToPadTo - existingString.Length);
-                  }
-
-                  stringBuilder.AppendFormat(
-                    $"  {PadRightHelper(variableValue.Name, 20)} :  {PadRightHelper(variableValue.Value.ToString(), 20)}: {variableValue.Type}");
-                  stringBuilder.AppendFormat($"{Environment.NewLine}");
-                }
-              }
-              catch
-              {
-                // all input type records will land here.
-                stringBuilder.Append("  Formatting Variables Error. Continuing...");
-                stringBuilder.AppendFormat($"{Environment.NewLine}");
-              }
-            }
-          }
-
-          _queryTimer.Stop();
-          stringBuilder.AppendFormat(
-            $"Ellapsed time for query is {_queryTimer.Elapsed.TotalMilliseconds:0.#} milliseconds.");
-          _logger.LogInformation(stringBuilder.ToString());
-        }
+        _logger.LogError(
+          new EventId(1, "GraphQL Error"),
+          error.Exception,
+          "GraphQL error in resolver {name}",
+          $"{context.Field.DeclaringType.Name}.{context.Field.Name}"
+        );
       }
+
+      base.ResolverError(context, error);
     }
   }
 }
