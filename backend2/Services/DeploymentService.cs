08fc7c64871b2a8a9305408ba4d3de155b3c5eea
Jan Kelin <jan@jkelin.com>
2021-03-09 08:10:51 +0100

    Add deploy funkctionality

--- backend2/Services/DeploymentService.cs
+++ backend2/Services/DeploymentService.cs
@@ -2,14 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using supercompose;
+using Z.EntityFramework.Plus;
 
 namespace backend2.Services
 {
   public class DeploymentService
   {
-    public async Task<Guid> Create(Guid node, Guid compose)
+    private readonly SupercomposeContext ctx;
+
+    public DeploymentService(SupercomposeContext ctx)
     {
-      throw new NotImplementedException();
+      this.ctx = ctx;
+    }
+
+    public async Task<Guid> Create(Guid nodeId, Guid composeId)
+    {
+      var nodeQuery = ctx.Nodes.Where(x => x.Id == nodeId).FutureValue();
+      var composeQuery = ctx.Composes.Where(x => x.Id == composeId).FutureValue();
+
+      var node = await nodeQuery.ValueAsync();
+      if (node == null) throw new NodeNotFoundException();
+
+      var compose = await composeQuery.ValueAsync();
+      if (compose == null) throw new ComposeNotFoundException();
+
+      var deployment = new Deployment
+      {
+        Id = Guid.NewGuid(),
+        Node = node,
+        Compose = compose,
+        Enabled = true,
+        LastDeployedVersionId = compose.CurrentId
+      };
+
+      await ctx.Deployments.AddAsync(deployment);
+      await ctx.SaveChangesAsync();
+
+      return deployment.Id.Value;
     }
   }
 }
