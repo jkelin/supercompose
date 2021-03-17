8619e78c7f8802d93dec54661fddba90c6ac19fd
Jan Kelin <jan@jkelin.com>
2021-03-17 08:15:32 +0100

    Refactor crypto service

--- backend2/Services/CryptoService.cs
+++ backend2/Services/CryptoService.cs
@@ -11,108 +11,105 @@ namespace supercompose
 {
   public class CryptoService
   {
-    private readonly IConfiguration configuration;
+    private readonly byte[] cryptoKey;
 
     private enum EncAlg
     {
-      AES_256_CBC = 1
+      // ReSharper disable once InconsistentNaming
+      // ReSharper disable once IdentifierTypo
+      AES_256_CBC_PKCS7 = 1
     }
 
     private enum KeyAlg
     {
+      // ReSharper disable once InconsistentNaming
       PBKDF2_SHA256_1000_32 = 1
     }
 
     [ProtoContract]
     private class EncryptedContainer
     {
-      [ProtoMember(1)] [Required] public EncAlg EncAlg { get; set; }
+      [ProtoMember(1)] [Required] public EncAlg EncAlg { get; init; }
 
-      [ProtoMember(2)] [Required] public KeyAlg KeyAlg { get; set; }
+      [ProtoMember(2)] [Required] public KeyAlg KeyAlg { get; init; }
 
-      [ProtoMember(3)] [Required] public byte[] IV { get; set; }
+      [ProtoMember(3)] [Required] public byte[] IV { get; init; }
 
       [ProtoMember(4)] [Required] public byte[] Data { get; set; }
     }
 
     public CryptoService(IConfiguration configuration)
     {
-      this.configuration = configuration;
+      cryptoKey = Encoding.UTF8.GetBytes(configuration["CryptoKey"]);
     }
 
-    private async Task<byte[]> Key(byte[] salt, int outputBytes)
+    private async Task<byte[]> DeriveKey(byte[] salt, int outputBytes, KeyAlg alg)
     {
-      var key = Encoding.UTF8.GetBytes(configuration["CryptoKey"]);
-      Rfc2898DeriveBytes k1 = new(key, salt, 1000, HashAlgorithmName.SHA256);
-      return k1.GetBytes(outputBytes);
+      Rfc2898DeriveBytes k1 = new(cryptoKey, salt, 1000, HashAlgorithmName.SHA256);
+      return await Task.Run(() => k1.GetBytes(outputBytes));
     }
 
     public async Task<byte[]> EncryptSecret(string secret)
     {
       using RNGCryptoServiceProvider rngCsp = new();
-      using Aes aesAlg = Aes.Create();
+      using Aes alg = Aes.Create();
 
-      aesAlg.Mode = CipherMode.CBC;
-      aesAlg.Padding = PaddingMode.PKCS7;
-      aesAlg.GenerateIV();
+      alg.Mode = CipherMode.CBC;
+      alg.Padding = PaddingMode.PKCS7;
+      alg.GenerateIV();
 
       var container = new EncryptedContainer
       {
-        EncAlg = EncAlg.AES_256_CBC,
-        KeyAlg = KeyAlg.PBKDF2_SHA256_1000_32
+        EncAlg = EncAlg.AES_256_CBC_PKCS7,
+        KeyAlg = KeyAlg.PBKDF2_SHA256_1000_32,
+        IV = alg.IV
       };
 
-      container.IV = aesAlg.IV;
-
-      aesAlg.Key = await Key(container.IV, aesAlg.KeySize / 8);
+      alg.Key = await DeriveKey(alg.IV, alg.KeySize / 8, container.KeyAlg);
 
       // Create an encryptor to perform the stream transform.
-      ICryptoTransform encryptor = aesAlg.CreateEncryptor();
+      using var transformer = alg.CreateEncryptor();
 
       // Create the streams used for encryption.
-      await using MemoryStream msEncrypt = new();
-      await using CryptoStream csEncrypt = new(msEncrypt, encryptor, CryptoStreamMode.Write);
-      await using StreamWriter swEncrypt = new(csEncrypt);
-      //Write all data to the stream.
-      await swEncrypt.WriteAsync(secret);
-      await swEncrypt.FlushAsync();
-      await csEncrypt.FlushFinalBlockAsync();
-      await csEncrypt.FlushAsync();
+      await using MemoryStream dataMs = new();
+      await using CryptoStream cs = new(dataMs, transformer, CryptoStreamMode.Write);
+      await using StreamWriter sw = new(cs);
 
-      container.Data = msEncrypt.ToArray();
+      await sw.WriteAsync(secret);
+      await sw.FlushAsync();
+      await cs.FlushFinalBlockAsync();
+      await cs.FlushAsync();
 
-      await using (MemoryStream ms = new())
-      {
-        Serializer.Serialize(ms, container);
+      container.Data = dataMs.ToArray();
 
-        return ms.ToArray();
-      }
+      await using MemoryStream containerMs = new();
+      Serializer.Serialize(containerMs, container);
+
+      return containerMs.ToArray();
     }
 
     public async Task<string> DecryptSecret(byte[] encrypted)
     {
-      await using MemoryStream ms = new(encrypted);
-      var container = Serializer.Deserialize<EncryptedContainer>(ms);
+      await using MemoryStream containerMs = new(encrypted);
+      var container = Serializer.Deserialize<EncryptedContainer>(containerMs);
 
       if (container == null) throw new InvalidOperationException("Secret is not a valid container");
 
-      using Aes aesAlg = Aes.Create();
+      using Aes alg = Aes.Create();
 
-      aesAlg.Mode = CipherMode.CBC;
-      aesAlg.Padding = PaddingMode.PKCS7;
-      aesAlg.IV = container.IV;
+      alg.Mode = CipherMode.CBC;
+      alg.Padding = PaddingMode.PKCS7;
+      alg.IV = container.IV;
+      alg.Key = await DeriveKey(container.IV, alg.KeySize / 8, container.KeyAlg);
 
-      aesAlg.Key = await Key(container.IV, aesAlg.KeySize / 8);
-
-      // Create a decryptor to perform the stream transform.
-      ICryptoTransform decryptor = aesAlg.CreateDecryptor();
+      var transformer = alg.CreateDecryptor();
 
       // Create the streams used for decryption.
-      await using MemoryStream msDecrypt = new(container.Data);
-      await using CryptoStream csDecrypt = new(msDecrypt, decryptor, CryptoStreamMode.Read);
-      using StreamReader srDecrypt = new(csDecrypt);
+      await using MemoryStream dataMs = new(container.Data);
+      await using CryptoStream cs = new(dataMs, transformer, CryptoStreamMode.Read);
+      using StreamReader sr = new(cs);
 
-      return await srDecrypt.ReadToEndAsync();
+      return await sr.ReadToEndAsync();
     }
   }
 }
